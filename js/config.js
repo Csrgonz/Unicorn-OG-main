e2983d8a803989fe1552c68330419929202b807b
Csrgonz <144561295+Csrgonz@users.noreply.github.com>
2023-11-19 21:46:23 -0600

    Id add

--- js/config.js
+++ js/config.js
@@ -2,9 +2,10 @@ window._config = {
     cognito: {
         userPoolId:         'us-east-2_oZ7XzeouY',         // e.g. us-east-2_oZ7XzeouY
         userPoolClientId:   '3l60r6vnh9dvmpa5ik3vit9fnc',         // e.g. 3l60r6vnh9dvmpa5ik3vit9fnc
-        region:             ''          // e.g. us-east-2
+        region:             'us-east-2'          // e.g. us-east-2
     },
     api: {
         invokeUrl:          ''          // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
     }
 };
+
