f625d797a90dca6091184e945f0d097456207362
Csrgonz <144561295+Csrgonz@users.noreply.github.com>
2023-11-19 21:35:07 -0600

    ID changes

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 window._config = {
     cognito: {
-        userPoolId:         '',         // e.g. us-east-2_uXboG5pAb
-        userPoolClientId:   '',         // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolId:         '',         // e.g. us-east-2_oZ7XzeouY
+        userPoolClientId:   '',         // e.g. 3l60r6vnh9dvmpa5ik3vit9fnc
         region:             ''          // e.g. us-east-2
     },
     api: {
